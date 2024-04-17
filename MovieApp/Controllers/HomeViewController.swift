0c1354160b83434fa6a1b0f405120b5b68fded6a
BKanayari <101924105+BKanayari@users.noreply.github.com>
2024-04-17 22:40:49 +0700

    Part 4: Add NavigationBarItem and Creating Section Header Title and custom it

--- MovieApp/Controllers/HomeViewController.swift
+++ MovieApp/Controllers/HomeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+  let sectionTitle: [String] = ["Trending", "Favorite", "New Realeases", "Recommeded for you", "Up Coming"]
+
   private let homeFeedTable: UITableView = {
     let table = UITableView(frame: .zero, style: .grouped)
     table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -25,17 +27,33 @@ class HomeViewController: UIViewController {
 
     let headerView = HeroHeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
     homeFeedTable.tableHeaderView = headerView
+
+    configureNavBar()
   }
 
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
     homeFeedTable.frame = view.bounds
   }
+
+  /// Creating NavBar Item
+  private func configureNavBar() {
+    var image = UIImage(named: "imgLogo")
+    image = image?.withRenderingMode(.alwaysOriginal)
+
+    navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+
+    navigationItem.rightBarButtonItems = [
+      UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil),
+      UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil)
+    ]
+    navigationController?.navigationBar.tintColor = .white
+  }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 20
+    return sectionTitle.count
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,7 +72,27 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     return 200
   }
 
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return sectionTitle[section]
+  }
+
   func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return 40
+  }
+
+  /// Create navigationBar dissapear effect
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    let defaultOffset = view.safeAreaInsets.top
+    let offset = scrollView.contentOffset.y + defaultOffset
+
+    navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+  }
+
+  func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    guard let header = view as? UITableViewHeaderFooterView else {return}
+    header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+    header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+    header.textLabel?.textColor = .white
+    header.textLabel?.text = header.textLabel?.text?.capitalized
   }
 }
